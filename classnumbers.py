bf31ee10a701a80b8492f8b7dee9568a368706e4
xcray476 <83043179+xcray476@users.noreply.github.com>
2021-07-29 22:59:28 -0500

    Add existing file

--- classnumbers.py
+++ classnumbers.py
@@ -9,16 +9,3 @@ while pstring:
 
 infile.close()
 
-content2 = []
-
-for elem in content:
-    elem2 = elem.lower()
-    content2.append(elem2)
-
-outfile = open("classnumbers.txt", "w")
-
-for elem3 in content2:
-    outfile.write(elem3 + '\n')
-
-
-outfile.close()
